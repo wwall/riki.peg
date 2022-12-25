31e865c2807e8f31fd95b3f52759d8d1c8a8b5c1
wwall <wwall@yandex.ru>
2022-12-25 16:30:42 +0300

    Сделан init

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @chcp 65001
 
 @rem Загружаем базу заглушку
-call vrunner init-dev --dt ./tools/clear.dt
+@call vrunner init-dev --dt ./tools/clear.dt
 
 @rem собрать внешние обработчики и отчеты в каталоге build
 @call vrunner compileepf src/epf/rikiPEG build %*
