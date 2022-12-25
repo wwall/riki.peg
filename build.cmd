8dfe05bae87002fe3fcb5621ba5de62832582ab2
wwall <wwall@yandex.ru>
2022-12-25 16:00:14 +0300

    Сделан init

--- build.cmd
+++ build.cmd
@@ -3,16 +3,6 @@
 @rem Загружаем базу заглушку
 call vrunner init-dev --dt ./tools/clear.dt
 
-@rem формирование файла конфигурации. для включения раскомментируйте код ниже
-@rem call vrunner compile --src src/cf --out build/1cv8.cf %*
-
-@rem обновление конфигурации основной разработческой ИБ из хранилища. для включения раскомментируйте код ниже
-@rem call vrunner loadrepo %*
-@rem call vrunner updatedb %*
-
 @rem собрать внешние обработчики и отчеты в каталоге build
-@rem call vrunner compileepf src/epf/МояВнешняяОбработка build %*
-@rem call vrunner compileepf src/erf/МойВнешнийОтчет build %*
-
-@rem собрать расширения конфигурации внутри ИБ
-@rem call vrunner compileext src/cfe/МоеРасширение МоеРасширение %*
+@call vrunner compileepf src/epf/rikiPEG build %*
+@call vrunner compileepf src/erf/tests/testRikiPeg build/test %*
