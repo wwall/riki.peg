7e9a7560b013f16fdbe95a70a505442f4af09599
wwall <wwall@yandex.ru>
2023-06-30 16:27:25 +0300

    Начаты тесты со строками

--- decompile.cmd
+++ decompile.cmd
@@ -1,6 +1,8 @@
 @chcp 65001
 
 @call vrunner decompileepf ./build/rikiPEG.epf  ./src/epf
+
+
 @call vrunner decompileepf ./build/example.epf  ./src/epf
 @call vrunner decompileepf ./build/tests/testRikiPeg.epf  ./tests/epf
 
